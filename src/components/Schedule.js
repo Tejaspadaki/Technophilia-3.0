9a065a0aaf1498b56c5d156acbe34a99dbcb0843
Tejas Padaki <136808428+Tejaspadaki@users.noreply.github.com>
2025-03-26 21:55:25 +0530

    update

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -4,27 +4,27 @@ const scheduleData = [
     {
         day: "Day 1 - Technophilia 3.0",
         events: [
-            { time: "9:00 AM - 10:00 AM", name: "Rangoli Competition", location: "A Block Atrium" },
+            { time: "9:00 AM - 10:00 AM", name: "colors & curls", location: "A Block Atrium" },
             { time: "10:30 AM - 12:00 PM", name: "Inauguration", location: "MBA Auditorium" },
-            { time: "12:00 PM - 3:00 PM", name: "Photo and Reel", location: "CR 1" },
-            { time: "1:00 PM - 4:30 PM", name: "Mobile Gaming (PUBG)", location: "Incubation B311" },
-            { time: "1:00 PM - 5:00 PM", name: "VR Gaming", location: "SDC B312" },
-            { time: "1:00 PM - 4:00 PM", name: "Cyber Security", location: "Flexible Room" },
-            { time: "2:00 PM - 4:00 PM", name: "Reverse Coding", location: "AI Lab" },
+            { time: "12:00 PM - 3:00 PM", name: "ReelGraphy", location: "CR 1" },
+            { time: "1:00 PM - 4:30 PM", name: "e-Sports (PUBG)", location: "Incubation B311" },
+            { time: "1:00 PM - 5:00 PM", name: "ImmersiPlay", location: "SDC B312" },
+            { time: "1:00 PM - 4:00 PM", name: "CyberQuest", location: "Flexible Room" },
+            { time: "2:00 PM - 4:00 PM", name: "Decode Derby", location: "AI Lab" },
             { time: "2:00 PM - 4:30 PM", name: "Treasure Hunt", location: "A Block Ground" },
         ]
     },
     {
         day: "Day 2 - Technophilia 3.0",
         events: [
-            { time: "9:00 AM - 10:00 AM", name: "Mehndi", location: "A Block Atrium" },
-            { time: "10:30 AM - 4:30 PM", name: "PC Gaming", location: "AI-R Lab" },
-            { time: "10:30 AM - 4:00 PM", name: "Web Redesign", location: "ADB Lab" },
-            { time: "10:30 AM - 4:00 PM", name: "AI Chatbot", location: "Flexible Room" },
-            { time: "1:00 PM - 4:00 PM", name: "Mobile Gaming (E-Football)", location: "Incubation B311" },
-            { time: "1:00 PM - 4:00 PM", name: "Startup Ideas", location: "CR-8" },
+            { time: "9:00 AM - 10:00 AM", name: "Mehndi Fiesta", location: "A Block Atrium" },
+            { time: "10:30 AM - 4:30 PM", name: "ValoArena", location: "AI-R Lab" },
+            { time: "10:30 AM - 4:00 PM", name: "WebReVise", location: "ADB Lab" },
+            { time: "10:30 AM - 4:00 PM", name: "BotArena", location: "Flexible Room" },
+            { time: "1:00 PM - 4:00 PM", name: "e-Sports (E-Football)", location: "Incubation B311" },
+            { time: "1:00 PM - 4:00 PM", name: "IgniteX ", location: "CR-8" },
             { time: "1:30 PM - 4:00 PM", name: "Treasure Hunt", location: "A Block Ground" },
-            { time: "1:00 PM - 4:00 PM", name: "VR Gaming", location: "SDC B312" },
+            { time: "1:00 PM - 4:00 PM", name: "ImmersiPlay", location: "SDC B312" },
             { time: "4:00 PM - 5:00 PM", name: "Validatory + Prize Distribution + Concert", location: "Flexible Room" }
         ]
     }
